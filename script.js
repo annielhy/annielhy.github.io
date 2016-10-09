76204bce6c0007b075b41a7b304b06652d03f4e3
annielhy <annielhy@gmail.com>
2016-10-10 00:14:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -152,6 +152,44 @@ function CenterControlBtnUndo(controlDiv, map) {
   });
 }
 
+
+// function to create Clear button
+function CenterControlBtnClear(controlDiv, map) {
+
+  // Set CSS for the control border.
+  var controlUI = document.createElement('div');
+  controlUI.style.backgroundColor = '#fff';
+  controlUI.style.border = '2px solid #fff';
+  controlUI.style.borderRadius = '3px';
+  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+  controlUI.style.cursor = 'pointer';
+  controlUI.style.marginBottom = '10px';
+  controlUI.style.textAlign = 'center';
+  controlUI.title = 'Click to recenter the map';
+  controlDiv.appendChild(controlUI);
+
+  // Set CSS for the control interior.
+  var controlText = document.createElement('div');
+  controlText.style.color = 'rgb(25,25,25)';
+  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+  controlText.style.fontSize = '12px';
+  controlText.style.lineHeight = '38px';
+  controlText.style.paddingLeft = '5px';
+  controlText.style.paddingRight = '5px';
+  controlText.innerHTML = 'Clear';
+  controlUI.appendChild(controlText);
+
+  // Setup the click event listeners: simply set the map to Chicago.
+  controlUI.addEventListener('click', function() {
+    while(allMarkers.length > 0) {
+      var lastMarker = allMarkers.pop();
+      var lastCircle = allCircles.pop();
+      lastMarker.setMap(null);
+      lastCircle.setMap(null);
+    }
+  });
+}
+
 // function to create Locate button
 function CenterControlBtnLocate(controlDiv, map) {
 
