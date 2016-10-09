8062c77e51ac952462deac344d6586dca3850950
annielhy <annielhy@gmail.com>
2016-10-10 01:40:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -393,7 +393,7 @@ function initMap() {
       } else if (drawColor == "Grey") {
         image = imageGrn;
         radius = 70;
-        fillColor = "#999999";
+        fillColor = "#666666";
       } else {
         image = imageRed;
         fillColor = "#FF0000";
